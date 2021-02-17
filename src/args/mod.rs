851852924552b6ccd5913a60778ede788ec0b0ec
robatipoor <mahdi.robatipoor@gmail.com>
2021-02-17 20:27:04 +0330

    add args mod

---
+++ src/args/mod.rs
@@ -0,0 +1,47 @@
+use clap::Clap;
+
+#[derive(Debug, Clap)]
+#[clap(
+    version = "0.1.0",
+    author = "Mahdi Robatipoor <mahdi.robatipoor@gmail.com>"
+)]
+pub struct Args {
+    #[clap(short, long)]
+    share_secret_key: String,
+    #[clap(short, long)]
+    ip_addr: String,
+    #[clap(short, long)]
+    ipv4_addr: String,
+    #[clap(short, long)]
+    ipv6_addr: String,
+    #[clap(short, long)]
+    port_numbers: Vec<u32>,
+    #[clap(short, long)]
+    password: String,
+    #[clap(short, long)]
+    is_sender: bool,
+    #[clap(short, long)]
+    debug_flag: bool,
+    #[clap(short, long)]
+    no_prompt: bool,
+    #[clap(short, long)]
+    only_local: bool,
+    #[clap(short, long)]
+    diable_local: bool,
+    #[clap(short, long)]
+    no_compress: bool,
+    #[clap(short, long)]
+    no_multiplexing: bool,
+    #[clap(short, long)]
+    std_out: bool,
+    #[clap(short, long)]
+    ignore_stdin: bool,
+    #[clap(short, long)]
+    sending_text: bool,
+    #[clap(short, long)]
+    ask: bool,
+}
+
+pub fn get_args() -> Args {
+    Args::parse()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use rock::server::Server;
 
+mod args;
+
+use args::get_args;
+
 #[tokio::main]
 async fn main() {
+    let _args = get_args();
     let server = Server::new("banner", "password");
     server.run("127.0.0.1:8080").await.unwrap();
     println!("rock cli tool");
